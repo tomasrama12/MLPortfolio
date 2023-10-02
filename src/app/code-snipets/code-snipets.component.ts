acd7246beff777c65b6ec0cc257d1bd4be1865cc
tomasrama12 <tomasramaviana@gmail.com>
2023-10-02 00:31:14 -0300

    d

--- src/app/code-snipets/code-snipets.component.ts
+++ src/app/code-snipets/code-snipets.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-code-snipets',
   templateUrl: './code-snipets.component.html',
   styleUrls: ['./code-snipets.component.css']
 })
 export class CodeSnipetsComponent {
-
+  constructor() { }
   @Input() code!: string;
   ngOnInit(){
     console.log(this.code)
-
   }
+  
 }
